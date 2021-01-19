b4f0b155b0e3f31a6a0ad3b68732be756ae78206
Okyop <pcombes80@gmail.com>
2021-01-19 12:38:17 +0100

    fin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from "react"
 
 function App() {
+  const [nombre, setNombre] = useState(0)
+  const [bolean, setBolean] = useState(true)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className={bolean ? "": "red"}>
+        {nombre}
+      </p>
+      <button onClick={()=>{
+       setNombre(nombre+1)
+      }}>
+        +1
+      </button>
+
+
+      <button onClick={()=>{
+       setNombre(nombre-1)
+      }}>
+        -1
+      </button>
+
+
+      <button onClick={()=>{
+        setBolean(!bolean)
+        console.log(bolean);
+      }}>
+        red
+      </button>
     </div>
   );
 }
